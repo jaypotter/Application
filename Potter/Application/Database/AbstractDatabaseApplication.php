6e96119561c89a203045bce46e5005e6ff760880
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-26 16:17:10 -0500

    use ApplicationTrait

--- Potter/Application/Database/AbstractDatabaseApplication.php
+++ Potter/Application/Database/AbstractDatabaseApplication.php
@@ -2,13 +2,16 @@
 
 namespace Potter\Application\Database;
 
-use Potter\{
-    Application\AbstractApplication,
-    DBAL\Database\DatabaseInterface    
+use Potter\Application\{
+    AbstractApplication,
+    ApplicationTrait
 };
+use Potter\DBAL\Database\DatabaseInterface;
 
 abstract class AbstractDatabaseApplication extends AbstractApplication implements DatabaseApplicationInterface
 {
+    use ApplicationTrait;
+    
     abstract public function getDatabase(): DatabaseInterface;
 
     abstract public function setDatabase(DatabaseInterface $database): void;
