e9b0da42cc28cc094857c7e35e124dc3ef26d8ea
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-26 16:28:31 -0500

    Update DatabaseApplication.php

--- Potter/Application/Database/DatabaseApplication.php
+++ Potter/Application/Database/DatabaseApplication.php
@@ -2,7 +2,15 @@
 
 namespace Potter\Application\Database;
 
+use Potter\DBAL\Database\DatabaseInterface;
+
 final class DatabaseApplication extends AbstractDatabaseApplication
 {
     use DatabaseApplicationTrait;
+
+    public function __construct(DatabaseInterface $database, string $name = self::DEFAULT_NAME)
+    {
+        $this->setDatabase($database);
+        parent::_construct($name);
+    }
 }
