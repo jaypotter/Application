d8ce14f7a4de9424d03a1fe51593db96e710e2f5
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-26 15:27:55 -0500

    method implementations

--- Potter/Application/Database/DatabaseApplicationTrait.php
+++ Potter/Application/Database/DatabaseApplicationTrait.php
@@ -2,7 +2,19 @@
 
 namespace Potter\Application\Database;
 
+use Potter\DBAL\Database\DatabaseInterface;
+
 trait DatabaseApplicationTrait
 {
-    
+    private DatabaseInterface $database;
+
+    public function getDatabase(): DatabaseInterface
+    {
+        return $this->database;
+    }
+
+    public function setDatabase(DatabaseInterface $database): void
+    {
+        $this->database = $database;
+    }
 }
