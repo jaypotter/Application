7b13d3e1f52dd562709278f4595d57e3da67fb32
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-26 16:08:01 -0500

    Application name

--- Potter/Application/AbstractApplication.php
+++ Potter/Application/AbstractApplication.php
@@ -4,5 +4,7 @@ namespace Potter\Application;
 
 abstract class AbstractApplication implements ApplicationInterface
 {
-    
+    abstract public function getName(): string;
+
+    abstract public function setName(string $name): void;
 }
