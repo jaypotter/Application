2d72813da3f429f8fff11363a771035c1544a0d5
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-26 16:17:01 -0500

    constructor

--- Potter/Application/Application.php
+++ Potter/Application/Application.php
@@ -5,4 +5,9 @@ namespace Potter\Application;
 final class Application extends AbstractApplication
 {
     use ApplicationTrait;
+
+    public function __construct(string $name = self::DEFAULT_NAME)
+    {
+        $this->setName($name);
+    }
 }
