3b4c7f8630a403d879ea0f6d3992d8e8a1c7b899
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-27 16:24:28 -0500

    Create SessionApplicationInterface.php

---
+++ Potter/Application/Session/SessionApplicationInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Potter\Application\Session;
+
+use Potter\Application\Database\DatabaseApplicationInterface;
+
+interface SessionApplicationInterface extends DatabaseApplicationInterface
+{
+
+}
