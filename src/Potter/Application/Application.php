651ce5ba3b865feb36f3efdbd97bf770de8e914f
Jay Potter <j@ypotter.ca>
2024-03-29 21:35:11 -0400

    Create Application.php

---
+++ src/Potter/Application/Application.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\Application;
+
+use \Potter\Aware\{AwareInterface, AwareTrait};
+use \Potter\Cloneable\{CloneableInterface, CloneableTrait};
+use \Potter\Container\ContainerInterface;
+use \Potter\Container\Aware\{ContainerAwareInterface, ContainerAwareTrait};
+use \Potter\Session\Aware\{SessionAwareInterface, SessionAwareTrait};
+
+final class Application extends AbstractApplication implements AwareInterface, CloneableInterface, ContainerAwareInterface, SessionAwareInterface
+{
+    use ApplicationTrait, AwareTrait, CloneableTrait, ContainerAwareTrait, SessionAwareTrait;
+    
+    public function __construct(ContainerInterface $container)
+    {
+        $this->setContainer($container);
+    }
+}
