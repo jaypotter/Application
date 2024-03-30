7a3b8b3172ae775245ae16f0d257a64adc043b88
Jay Potter <j@ypotter.ca>
2024-03-30 12:23:18 -0400

    OutputBufferAware

--- src/Potter/Application/Application.php
+++ src/Potter/Application/Application.php
@@ -6,13 +6,13 @@ namespace Potter\Application;
 
 use \Potter\Aware\{AwareInterface, AwareTrait};
 use \Potter\Cloneable\{CloneableInterface, CloneableTrait};
-use \Potter\Container\ContainerInterface;
-use \Potter\Container\Aware\{ContainerAwareInterface, ContainerAwareTrait};
+use \Potter\Container\ContainerInterface, \Potter\Container\Aware\{ContainerAwareInterface, ContainerAwareTrait};
+use \Potter\Buffer\Output\Aware\{OutputBufferAwareInterface, OutputBufferAwareTrait};
 use \Potter\Session\Aware\{SessionAwareInterface, SessionAwareTrait};
 
-final class Application extends AbstractApplication implements AwareInterface, CloneableInterface, ContainerAwareInterface, SessionAwareInterface
+final class Application extends AbstractApplication implements AwareInterface, CloneableInterface, ContainerAwareInterface, OutputBufferAwareInterface, SessionAwareInterface
 {
-    use ApplicationTrait, AwareTrait, CloneableTrait, ContainerAwareTrait, SessionAwareTrait;
+    use ApplicationTrait, AwareTrait, CloneableTrait, ContainerAwareTrait, OutputBufferAwareTrait, SessionAwareTrait;
     
     public function __construct(ContainerInterface $container)
     {
