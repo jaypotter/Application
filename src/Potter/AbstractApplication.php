8422be725cd2fdcc474354b4b66fc59325e7af04
Jay Potter <j@ypotter.ca>
2026-08-29 16:33:58 -0400

    create AbstractApplication

---
+++ src/Potter/AbstractApplication.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter;
+
+abstract class AbstractApplication 
+    extends \Symfony\Component\Console\Application
+    implements ApplicationInterface
+{ }
